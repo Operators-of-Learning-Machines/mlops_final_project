a185279c19be6577f919d16c8a9ee52e64bb5b81
Oskar Kocjančič <52063496+OskarKocjancic@users.noreply.github.com>
2026-01-09 17:01:42 +0100

    add starting model, training

---
+++ src/sclera_identity_classification/squeezenet.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+from torchvision.models import squeezenet1_1, SqueezeNet1_1_Weights
+
+class SqueezeNet(nn.Module):
+    def __init__(self, pretrained=False, transfer_learning_model_path=None, classes=220):
+        super(SqueezeNet, self).__init__()
+        if transfer_learning_model_path is not None:
+            self.model = torch.load(transfer_learning_model_path)
+        else:
+            self.model = squeezenet1_1(weights=SqueezeNet1_1_Weights.IMAGENET1K_V1 if pretrained else None)
+
+        self.model.classifier[1] = nn.Conv2d(self.model.classifier[1].in_channels, classes, 1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.model.features(x)
+        x = self.model.classifier(x)
+        return torch.flatten(x, 1)
+
+
