d8581b12ece0934154e40d99418e1262f3df2b1f
Oskar Kocjančič <52063496+OskarKocjancic@users.noreply.github.com>
2026-01-12 16:11:05 +0100

    feat: add config files for reproducibility

--- src/sclera_identity_classification/squeezenet.py
+++ src/sclera_identity_classification/squeezenet.py
@@ -3,14 +3,14 @@ from torch import nn
 from torchvision.models import squeezenet1_1, SqueezeNet1_1_Weights
 
 class SqueezeNet(nn.Module):
-    def __init__(self, pretrained=False, transfer_learning_model_path=None, classes=220):
+    def __init__(self, pretrained=False, transfer_learning_model_path=None, out_channels=220, kernel_size=1):
         super(SqueezeNet, self).__init__()
         if transfer_learning_model_path is not None:
             self.model = torch.load(transfer_learning_model_path)
         else:
             self.model = squeezenet1_1(weights=SqueezeNet1_1_Weights.IMAGENET1K_V1 if pretrained else None)
 
-        self.model.classifier[1] = nn.Conv2d(self.model.classifier[1].in_channels, classes, 1)
+        self.model.classifier[1] = nn.Conv2d(self.model.classifier[1].in_channels, out_channels=out_channels, kernel_size=kernel_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.model.features(x)
