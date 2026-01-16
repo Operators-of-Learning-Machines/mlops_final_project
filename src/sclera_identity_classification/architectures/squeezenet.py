273b9d45976dcf54696ee0998e34eb162a5c9862
AmalieSommer <amaliesommer@itkg.dk>
2026-01-16 15:54:52 +0100

    add api

--- src/sclera_identity_classification/architectures/squeezenet.py
+++ src/sclera_identity_classification/architectures/squeezenet.py
@@ -3,11 +3,12 @@ from torch import nn
 from torchvision.models import squeezenet1_1, SqueezeNet1_1_Weights
 
 
+
 class SqueezeNet(nn.Module):
     def __init__(self, pretrained=False, transfer_learning_model_path=None, out_channels=220, kernel_size=1):
         super(SqueezeNet, self).__init__()
         if transfer_learning_model_path is not None:
-            self.model = torch.load(transfer_learning_model_path)
+            self.model = torch.load(transfer_learning_model_path, weights_only=False)
         else:
             self.model = squeezenet1_1(weights=SqueezeNet1_1_Weights.IMAGENET1K_V1 if pretrained else None)
 
