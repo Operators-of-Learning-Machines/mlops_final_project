0a7302feea8f2062da5fdbbb749acc87734a8e83
asold <asoldan1459@gmail.com>
2026-01-22 15:00:26 +0100

    Added prometheus monitoring of metrics

--- api/main.py
+++ api/main.py
@@ -9,12 +9,28 @@ from PIL import Image, UnidentifiedImageError
 from PIL.Image import DecompressionBombError
 from models import onnx as onnx_module
 from contextlib import asynccontextmanager
-
+import time
+from prometheus_client import Counter, Histogram, make_asgi_app
 
 
 CHANNELS = 3 # model input channels
 MODEL_PATH = "models/model.pth" # statically set to the sclera model (note modify if multiple models are added)
 MODEL_ONNX_PATH = "models/model.onnx"
+
+root_counter = Counter("root_call", "Number of calls to the root endpoint")
+successful_inference_counter = Counter("successful_inference", "Number of successfull calls to the inference endpoint")
+failed_inference_counter = Counter("failed_inference", "Number of failed calls to the inference endpoint")
+inference_requests_total = Counter(
+    "sclera_inference_requests_total",
+    "Total number of inference requests"
+)
+
+# Latency histogram (seconds)
+inference_latency_seconds = Histogram(
+    "sclera_inference_latency_seconds",
+    "Time spent processing inference requests",
+    buckets=(0.05, 0.1, 0.25, 0.5, 1, 2, 5)
+)
 
 
 @asynccontextmanager
@@ -40,18 +56,25 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.mount("/metrics", make_asgi_app())
 
 
 @app.get("/")
 def root():
+    root_counter.inc()
     return {
         "message": "Welcome to the Sclera Identity Classification inference API!",
         "status": HTTPStatus.OK
     }
 
 
+
 @app.post("/sclera_model")
 async def sclera_model(file: UploadFile = File()):
+
+    inference_requests_total.inc()
+    start_time = time.perf_counter()
+    
     try:
 
         img = await file.read()
@@ -60,11 +83,13 @@ async def sclera_model(file: UploadFile = File()):
             pil_img = Image.open(io.BytesIO(img))
             pil_img.load()  # force decoding now
         except UnidentifiedImageError:
+            failed_inference_counter.inc()
             raise HTTPException(
                 status_code=400,
                 detail="Invalid image format. Please upload a valid PNG."
             )
         except DecompressionBombError:
+            failed_inference_counter.inc()
             raise HTTPException(
                 status_code=400,
                 detail="Image is too large."
@@ -92,10 +117,14 @@ async def sclera_model(file: UploadFile = File()):
             {"input": input_np}
         )[0]
 
+        successful_inference_counter.inc()
+
         return {
             "result": output.flatten().tolist(),
             "status": HTTPStatus.OK
         }
 
     finally:
+        elapsed = time.perf_counter() - start_time
+        inference_latency_seconds.observe(elapsed)
         file.file.close()
